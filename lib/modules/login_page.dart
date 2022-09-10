55f76dd77d0b93e8b91b95378d56bf194ce9a46c
JaeKyuP <106376249+JaeKyuP@users.noreply.github.com>
2022-09-10 23:38:03 +0900

    Update login_page.dart

--- lib/modules/login_page.dart
+++ lib/modules/login_page.dart
@@ -18,9 +18,9 @@ class LoginPage extends StatelessWidget{
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   SizedBox(height: 50,),
-                  Text("Welcome,",style: TextStyle(fontSize: 26,fontWeight: FontWeight.bold),),
+                  Text("환영합니다,",style: TextStyle(fontSize: 26,fontWeight: FontWeight.bold),),
                   SizedBox(height: 6,),
-                  Text("Sign in to continue!",style: TextStyle(fontSize: 20,color: Colors.grey.shade400),),
+                  Text("진행하기 위해 가입하세요!",style: TextStyle(fontSize: 20,color: Colors.grey.shade400),),
                 ],
               ),
               Column(
@@ -64,8 +64,8 @@ class LoginPage extends StatelessWidget{
                   ),
                   SizedBox(height: 12,),
                   Align(
-                    alignment: Alignment.topRight,
-                    child: Text("비밀번호를 까먹었나요?",style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
+                    alignment: Alignment.topLeft,
+                    child: Text("아이디 찾기 / 비밀번호 찾기",style: TextStyle(fontSize: 14,fontWeight: FontWeight.w600),),
                   ),
                   SizedBox(height: 30,),
                   Container(
@@ -101,46 +101,136 @@ class LoginPage extends StatelessWidget{
                       ),
                     ),
                   ),
-                  SizedBox(height: 16,),
-                  Container(
-                    height: 50,
-                    width: double.infinity,
-                    child: TextButton(
-                      onPressed: (){},
-                      style: TextButton.styleFrom(
-                        textStyle: TextStyle(
-                          color: Colors.indigo.shade50,
+                  SizedBox(height: 15,),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        height: 50,
+                        width: double.infinity,
+                        child: TextButton(
+                          onPressed: (){},
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.zero,
+                              )
+                            )
+                          ),
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                              gradient: LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                                colors: [
+                                Color(0xffff5f6d),
+                                Color(0xffff5f6d),
+                                Color(0xffffc371),
+                                ],
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Image.asset("images/facebook.png",height: 18,width: 18,),
+                                SizedBox(width: 10, height: 50),
+                                Text("페이스북으로 로그인하기",style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                              ],
+                            ),
+                          ),
                         ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6),
-                        )
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Image.asset("images/facebook.png",height: 18,width: 18,),
-                          SizedBox(width: 10,),
-                          Text("페이스북으로 로그인하기",style: TextStyle(color: Colors.indigo,fontWeight: FontWeight.bold),),
-                        ],
+                      SizedBox(height: 15,),
+                      Container(
+                        height: 50,
+                        width: double.infinity,
+                        child: TextButton(
+                          onPressed: (){},
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.zero,
+                              )
+                            )
+                          ),
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                              gradient: LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                                colors: [
+                                Color(0xffff5f6d),
+                                Color(0xffff5f6d),
+                                Color(0xffffc371),
+                                ],
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Image.asset("images/naver.png",height: 18,width: 18,),
+                                SizedBox(width: 10, height: 50),
+                                Text("네이버로 로그인하기",style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                              ],
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                      SizedBox(height: 15,),
+                      Container(
+                        height: 50,
+                        width: double.infinity,
+                        child: TextButton(
+                          onPressed: (){},
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.zero,
+                              )
+                            )
+                          ),
+                          child: Ink(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6),
+                              gradient: LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                                colors: [
+                                Color(0xffff5f6d),
+                                Color(0xffff5f6d),
+                                Color(0xffffc371),
+                                ],
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Image.asset("images/kakao.png",height: 18,width: 18,),
+                                SizedBox(width: 10, height: 50),
+                                Text("카카오톡으로 로그인하기",style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15,),       
+                    ],
                   ),
-                  SizedBox(height: 30,),
-                ],
+                ],         
               ),
               Padding(
                 padding: EdgeInsets.only(bottom: 10),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text("I'm a new user.",style: TextStyle(fontWeight: FontWeight.bold),),
                     GestureDetector(
                       onTap: (){
                         Navigator.push(context, MaterialPageRoute(builder: (context){
                           return SignupPage();
                         }));
                       },
-                      child: Text("Sign up",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red),),
+                      child: Text("회원가입",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red),),
                     )
                   ],
                 ),
